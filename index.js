c23b5d91d913a060a026ec9e5c4c32d3c78aca1d
yuri-costa-flooxai <yuri.costa.flooxai@gmail.com>
2025-04-09 13:22:21 -0300

    feat: variables and selectors created.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-alert("Olá mundo")
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelector("#form-user-create [name=gender]:checked");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#exampleInputAdmin");
