7eb6d71b20055e42c0c8a88598a040722b4fa1ad
yuri-costa-flooxai <yuri.costa.flooxai@gmail.com>
2025-04-09 13:26:49 -0300

    feat: For Each iteration in index.js.

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ var email = document.querySelector("#exampleInputEmail");
 var password = document.querySelector("#exampleInputPassword");
 var photo = document.querySelector("#exampleInputFile");
 var admin = document.querySelector("#exampleInputAdmin");
+
+
+var fields = document.querySelectorAll("#form-user-create [name]")
+
+fields.forEach(function(field, index){
+
+    console.log(field.id, field.name, field.value, field.checked, index)
+
+});
