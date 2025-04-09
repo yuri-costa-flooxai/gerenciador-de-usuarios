abe8b6af240ef459a3d21b76125a25813bbb9f43
yuri-costa-flooxai <yuri.costa.flooxai@gmail.com>
2025-04-09 13:46:51 -0300

    refactor: removing useless variables.

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
-var name = document.querySelector("#exampleInputName");
-var gender = document.querySelector("#form-user-create [name=gender]:checked");
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");
-var password = document.querySelector("#exampleInputPassword");
-var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
-
-
-
-
 var fields = document.querySelectorAll("#form-user-create [name]")
+var user = {}
 
 fields.forEach(function(field, index){
 
     if (field.name === "gender") {
 
         if (field.checked) {
-
-            console.log("sim", field)
-            
+            user.gender
+        
         }
 
     } else {
-
-        console.log("nao")
+        user[field.name] = field.value
 
     }
-
 });
