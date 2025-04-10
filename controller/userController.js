85e7938e21065a6fad6409f824e3030ce46db084
yuri-costa-flooxai <yuri.costa.flooxai@gmail.com>
2025-04-10 12:33:13 -0300

    refactor: getters and setters in User model.

--- controller/userController.js
+++ controller/userController.js
@@ -15,6 +15,10 @@ class UserController {
 
             event.preventDefault();
 
+            let btn = this.formEl.querySelector("[type=submit]");
+
+            btn.disabled = true;
+
             let values = this.getValues();
 
             this.getPhoto().then(
@@ -23,6 +27,10 @@ class UserController {
                     values.photo = content;
 
                     this.addLine(values);
+
+                    this.formEl.reset();
+
+                    btn.disabled = false;
 
                 }, 
                 (e) => {
@@ -120,7 +128,7 @@ class UserController {
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
-                <td>${dataUser.birth}</td>
+                <td>${dataUser.register}</td>
                 <td>
                     <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
@@ -131,6 +139,4 @@ class UserController {
         this.tableEl.appendChild(tr);
 
     }
-
-
 }
