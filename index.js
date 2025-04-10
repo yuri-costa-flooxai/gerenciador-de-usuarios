31f6d0246c9b0b222d239b95ba86a929408bf3a9
yuri-costa-flooxai <yuri.costa.flooxai@gmail.com>
2025-04-10 10:08:54 -0300

    feat: submit form event

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-var fields = document.querySelectorAll("#form-user-create [name]")
-var user = {}
+var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {}; 
 
-fields.forEach(function(field, index){
+document.getElementById("form-user-create").addEventListener("submit", function(event){
 
-    if (field.name === "gender") {
+    event.preventDefault();
 
-        if (field.checked) {
-            user.gender
-        
+    fields.forEach(function(field, index){
+
+        if (field.name === "gender") {
+
+            if (field.checked) {
+                user[field.name] = field.value
+            }
+
+        } else {
+            user[field.name] = field.value
         }
 
-    } else {
-        user[field.name] = field.value
+    });
 
-    }
+    console.log(user);
+
 });
