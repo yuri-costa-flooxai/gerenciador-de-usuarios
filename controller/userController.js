7e7893526884ffde788ca88f1da532570cdbcc22
yuri-costa-flooxai <yuri.costa.flooxai@gmail.com>
2025-04-10 11:16:08 -0300

    feat: files operations.

--- controller/userController.js
+++ controller/userController.js
@@ -15,9 +15,41 @@ class UserController {
 
             event.preventDefault();
 
-            this.addLine(this.getValues())
+            let values = this.getValues();
+
+            this.getPhoto((content) => {
+
+                values.photo = content;
+
+                this.addLine(values);
+            });
+
         
         });
+
+    }
+
+    getPhoto(){
+
+        let fileReader = new FileReader();
+
+        let elements = [...this.formEl.elements].filter(item => {
+
+            if (item.name === 'photo') {
+                return item;
+            }
+
+        });
+
+        let file = elements[0].files[0];
+
+        fileReader.onload = () => {
+
+            callback(fileReader.result);
+
+        };
+
+        fileReader.readAsDataURL(file);
 
     }
 
@@ -59,7 +91,7 @@ class UserController {
 
         this.tableEl.innerHTML = `
             <tr>
-                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${dataUser.admin}</td>
