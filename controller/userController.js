460282264a76d07301b865069e616541954b17f2
yuri-costa-flooxai <yuri.costa.flooxai@gmail.com>
2025-04-10 13:10:51 -0300

    feat: forms validation.
    Now the user has to fill every mandatory field of the form.

--- controller/userController.js
+++ controller/userController.js
@@ -21,6 +21,8 @@ class UserController {
 
             let values = this.getValues();
 
+            if (!values) return false;
+
             this.getPhoto().then(
                 (content) => {
 
@@ -83,8 +85,16 @@ class UserController {
     getValues(){
 
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach(function(field, index){
+
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
+
+                field.parentElement.classList.add("has-error");
+                isValid = false
+
+            }
 
             if (field.name === "gender") {
     
@@ -103,6 +113,10 @@ class UserController {
             }
     
         });
+
+        if (!isValid) {
+            return false;
+        }
     
         return new User(
             user.name, 
